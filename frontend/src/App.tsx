1413f6ac4dd58631448a6f8f5225ea3ab9adfb1c
imspdr <konglsh@naver.com>
2024-12-12 21:16:48 +0900

    add grid line

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { observer } from "mobx-react";
 import { useRootStore } from "@src/store/RootStoreProvider";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useState, useEffect } from "react";
-import { Typography } from "@mui/material";
+import { StyledEngineProvider, Typography } from "@mui/material";
 import { css } from "@emotion/react";
 import { unselectable } from "@src/util";
 import ThemeToggle from "@src/components/ThemeToggle";
@@ -37,6 +37,10 @@ function App() {
     const light = styles.getPropertyValue("--light");
     const mint = styles.getPropertyValue("--mint");
     const pink = styles.getPropertyValue("--pink");
+    const red = styles.getPropertyValue("--red");
+    const blue = styles.getPropertyValue("--blue");
+    const gray = styles.getPropertyValue("--gray");
+    const grid = styles.getPropertyValue("--grid");
     const scrollColorBlack = styles.getPropertyValue("--scroll-color-black");
 
     //dark
@@ -44,6 +48,10 @@ function App() {
     const darkWhite = styles.getPropertyValue("--dark-white");
     const darkMint = styles.getPropertyValue("--dark-mint");
     const darkPink = styles.getPropertyValue("--dark-pink");
+    const darkBlue = styles.getPropertyValue("--dark-blue");
+    const darkRed = styles.getPropertyValue("--dark-red");
+    const darkGray = styles.getPropertyValue("--dark-gray");
+    const darkGrid = styles.getPropertyValue("--dark-grid");
     const scrollColorWhite = styles.getPropertyValue("--scroll-color-white");
 
     const docEl = document.documentElement;
@@ -54,6 +62,10 @@ function App() {
       docEl.style.setProperty("--highlight", mint);
       docEl.style.setProperty("--paper", white);
       docEl.style.setProperty("--warning", pink);
+      docEl.style.setProperty("--chart-red", red);
+      docEl.style.setProperty("--chart-blue", blue);
+      docEl.style.setProperty("--chart-gray", gray);
+      docEl.style.setProperty("--chart-grid", grid);
     } else {
       docEl.style.setProperty("--background", darkBlack);
       docEl.style.setProperty("--foreground", darkWhite);
@@ -61,6 +73,10 @@ function App() {
       docEl.style.setProperty("--highlight", darkMint);
       docEl.style.setProperty("--paper", black);
       docEl.style.setProperty("--warning", darkPink);
+      docEl.style.setProperty("--chart-red", darkRed);
+      docEl.style.setProperty("--chart-blue", darkBlue);
+      docEl.style.setProperty("--chart-gray", darkGray);
+      docEl.style.setProperty("--chart-grid", darkGrid);
     }
     setDarkMode((v) => !v);
   };
--- frontend/src/components/TimeseriesChart.tsx
+++ frontend/src/components/TimeseriesChart.tsx
@@ -1,7 +1,7 @@
 import { css } from "@emotion/react";
 import { Circle } from "@mui/icons-material";
 import { GivenData, PredictedData } from "@src/store/types";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export default function TimeseriesChart(props: {
   givenData: GivenData[];
@@ -10,29 +10,55 @@ export default function TimeseriesChart(props: {
   const [divided, setDivided] = useState(false);
   const [scale, setScale] = useState(1);
 
-  const [window, setWindow] = useState(0);
+  const [nowIndex, setNowIndex] = useState(0);
 
   const [transitionOn, setTransitionOn] = useState(false);
   const [scrolling, setScrolling] = useState(false);
   const [startX, setStartX] = useState(0);
 
+  useEffect(() => {
+    setNowIndex((v) => Math.min(length - scaledLength, v));
+  }, [scale]);
+  useEffect(() => {
+    setTransitionOn(false);
+  }, [divided]);
+
+  // add key down scroll effect
+  const keyDownEvent = function (ev: KeyboardEvent) {
+    if (ev.key === "ArrowRight") {
+      setNowIndex((v) => Math.min(v + 1, length - length / scale));
+    } else if (ev.key === "ArrowLeft") {
+      setNowIndex((v) => Math.max(0, v - 1));
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", keyDownEvent);
+    return () => {
+      window.removeEventListener("keydown", keyDownEvent);
+    };
+  }, [scale]);
+
   const width = 1600;
   const height = 1000;
   const padding = 50;
-  const scrollWidth = 10;
+  const leftPadding = 50;
+  const rightPadding = 100;
+  const scrollWidth = 5;
 
   const predictedColor = "var(--highlight)";
-  const givenColor = "blue";
+  const givenColor = "var(--chart-gray)";
 
+  // select data using state
   const length = props.predictedData.length;
-  const scaledLength = props.predictedData.length / scale;
+  const scaledLength = Math.floor(length / scale);
 
-  const selectedGivenData = props.givenData.slice(window, window + scaledLength);
-  const selectedPredictedData = props.predictedData.slice(window, window + scaledLength);
+  const selectedGivenData = props.givenData.slice(nowIndex, nowIndex + scaledLength);
+  const selectedPredictedData = props.predictedData.slice(nowIndex, nowIndex + scaledLength);
 
   const givenPrediction = selectedPredictedData.filter((data) => data.predicted == 0);
   const predictedData = selectedPredictedData.filter((data) => data.predicted == 1);
 
+  // calc min max value to get proper size
   const maxY = Math.max(
     ...selectedPredictedData.map((d) => d.yhat_upper),
     ...selectedGivenData.map((d) => d.upper)
@@ -49,34 +75,49 @@ export default function TimeseriesChart(props: {
     ...selectedPredictedData.map((d) => d.trend_lower),
     ...selectedGivenData.map((d) => d.lower)
   );
-  const maxAbsAdditive = Math.max(...props.predictedData.map((d) => Math.abs(d.additive_terms)));
+  const maxAbsAdditive = Math.ceil(
+    Math.max(...props.predictedData.map((d) => Math.abs(d.additive_terms)))
+  );
 
+  // functions to transform coordinate to svg's
   const xScale = (x: number) =>
-    ((x - window) / (scaledLength - 1)) * (width - 2 * padding) + padding;
-  const yScale = (y: number) =>
+    ((x - nowIndex) / (scaledLength - 1)) * (width - leftPadding - rightPadding) + leftPadding;
+  const baseScale = (y: number) =>
     height - padding - ((y - minY) / (maxY - minY)) * (height - 2 * padding);
 
   const trendScale = (y: number) =>
-    height / 2 -
-    padding / 2 -
-    (((y - minTrend) / (maxTrend - minTrend)) * (height - 3 * padding)) / 2;
+    height / 2 - padding - ((y - minTrend) / (maxTrend - minTrend)) * (height / 2 - 2 * padding);
   const additiveScale = (y: number) =>
-    height - padding - ((y / maxAbsAdditive + 1) * (height - 3 * padding)) / 4;
+    height -
+    padding -
+    (height / 2 - padding * 2) / 2 -
+    (y / maxAbsAdditive) * (height / 4 - padding);
 
-  const xAxis = [padding, height - padding, height / 2 - padding / 2, height / 2 + padding / 2];
-  const yAxis = [padding, width - padding];
-
+  const yScale = (y: number) => {
+    if (divided) {
+      return trendScale(y);
+    } else {
+      return baseScale(y);
+    }
+  };
+  // values for grid
+  const yGap = Math.round((maxY - minY) / 8);
+  const xAxis = [leftPadding, width - rightPadding];
+  const yAxis = [...new Array(9)].map((_, i) => {
+    return Math.round(divided ? minTrend : minY) + i * yGap;
+  });
+  const additiveYAxis = [-maxAbsAdditive, 0, maxAbsAdditive];
   const datas = [
     {
       name: "given",
       data: givenPrediction,
-      start: window,
+      start: nowIndex,
       color: givenColor,
     },
     {
       name: "predicted",
       data: predictedData,
-      start: window + givenPrediction.length,
+      start: nowIndex + givenPrediction.length,
       color: predictedColor,
     },
   ];
@@ -92,31 +133,11 @@ export default function TimeseriesChart(props: {
           {" "}
           divide
         </div>
-
-        <div
-          onClick={() => {
-            setTransitionOn(false);
-            setScale((v) => (v > 1 ? v / 2 : 1));
-          }}
-        >
-          {" "}
-          -
-        </div>
-        <div
-          onClick={() => {
-            setTransitionOn(false);
-            setScale((v) => (v > 8 ? 1 : v * 2));
-          }}
-        >
-          {" "}
-          +
-        </div>
       </div>
       <div
         css={css`
           width: 90%;
           max-width: 800px;
-          aspect-ratio: 1.6 / 1;
           background-color: var(--paper);
           cursor: ${scrolling ? "grabbing" : "grab"};
           .data-transition {
@@ -137,72 +158,84 @@ export default function TimeseriesChart(props: {
           setScrolling(false);
         }}
         onMouseMove={(e) => {
-          if (scrolling) {
-            setWindow((v) => {
-              if (e.clientX < startX) {
-                return Math.min(v + 1, length - scaledLength);
-              } else {
-                return Math.max(0, v - 1);
-              }
+          if (scrolling && scale > 1) {
+            setNowIndex((v) => {
+              return Math.min(
+                Math.max(
+                  0,
+                  v + Math.floor(((startX - e.clientX) / window.innerWidth) * scaledLength)
+                ),
+                length - scaledLength
+              );
             });
           }
         }}
-        onFocus={() => {
+        onWheel={(e) => {
+          if (e.deltaY < 0) {
+            setTransitionOn(false);
+            setScale((v) => (v > 8 ? 1 : v * 2));
+          } else {
+            setTransitionOn(false);
+            setScale((v) => (v > 1 ? v / 2 : 1));
+          }
+        }}
+        onMouseLeave={() => {
           setScrolling(false);
         }}
       >
         <svg viewBox={`0 0 ${width} ${height}`}>
-          {scale > 1 && (
-            <rect
-              x={(window / length) * width}
-              y={height - scrollWidth}
-              height={scrollWidth}
-              width={width / scale}
-              fill="var(--foreground)"
-            />
-          )}
-          {/* {xAxis.map((y) => (
-          <path d={`M ${yAxis[0]} ${y} L ${yAxis[1]} ${y}`} stroke="var(--foreground)" />
-        ))} */}
-          {/* givenData dots */}
-          {selectedGivenData.map((data: GivenData, i: number) => {
-            const realIndex = window + i;
-            return (
-              <>
-                {/* <circle
-                className="cy-transition"
-                cx={xScale(i)}
-                cy={divided ? trendScale(data.y) : yScale(data.y)}
-                r={3}
+          {/* {grid line} */}
+          {yAxis.map((y) => (
+            <>
+              <path
+                d={`M ${xAxis[0]} ${yScale(y)} L ${xAxis[1]} ${yScale(y)}`}
+                stroke="var(--chart-grid)"
+                strokeWidth={1}
+                className="y-transition"
+              />
+              <text
+                x={width - (rightPadding * 2) / 3}
+                y={yScale(y) + 5}
+                fontSize={20}
                 fill={"var(--foreground)"}
-              /> */}
-                <rect
-                  className="y-transition"
-                  x={xScale(realIndex) - ((1 / (scaledLength - 1)) * (width - 2 * padding)) / 4}
-                  y={
-                    divided
-                      ? trendScale(Math.max(data.y, data.start))
-                      : yScale(Math.max(data.y, data.start))
-                  }
-                  height={Math.max(1, Math.abs(yScale(data.start) - yScale(data.y)))}
-                  width={((1 / (scaledLength - 1)) * (width - 2 * padding)) / 2}
-                  fill={data.start > data.y ? "blue" : "red"}
-                />
-                <rect
-                  className="y-transition"
-                  x={xScale(realIndex)}
-                  y={divided ? trendScale(data.upper) : yScale(data.upper)}
-                  height={
-                    divided
-                      ? trendScale(data.lower) - trendScale(data.upper)
-                      : yScale(data.lower) - yScale(data.upper)
-                  }
-                  width={1}
-                  fill={data.start > data.y ? "blue" : "red"}
-                />
-              </>
-            );
-          })}
+                className="y-transition"
+              >
+                {y}
+              </text>
+            </>
+          ))}
+          {divided && (
+            <>
+              <rect
+                x={0}
+                y={height / 2 - padding / 2}
+                height={padding}
+                width={width}
+                fill={"var(--background)"}
+              />
+              {additiveYAxis.map((y) => {
+                return (
+                  <>
+                    <path
+                      d={`M ${xAxis[0]} ${additiveScale(y)} L ${xAxis[1]} ${additiveScale(y)}`}
+                      stroke="var(--chart-grid)"
+                      strokeWidth={1}
+                      className="y-transition"
+                    />
+                    <text
+                      x={width - (rightPadding * 2) / 3}
+                      y={additiveScale(y) + 5}
+                      fontSize={20}
+                      fill={"var(--foreground)"}
+                      className="y-transition"
+                    >
+                      {y}
+                    </text>
+                  </>
+                );
+              })}
+            </>
+          )}
           {/* main line  */}
           {datas.map((dataType) => {
             return (
@@ -219,12 +252,7 @@ export default function TimeseriesChart(props: {
                       .join(" ")} ${dataType.data
                       .slice()
                       .reverse()
-                      .map(
-                        (d) => `L ${xScale(d.index)} ${trendScale(d.trend_lower)}
-                  `
-                      )}
-                  Z
-                `}
+                      .map((d) => `L ${xScale(d.index)} ${trendScale(d.trend_lower)}`)} Z`}
                     fill={dataType.color}
                     opacity="0.3"
                   />
@@ -240,12 +268,7 @@ export default function TimeseriesChart(props: {
                       .join(" ")} ${dataType.data
                       .slice()
                       .reverse()
-                      .map(
-                        (d) => `L ${xScale(d.index)} ${yScale(d.yhat_lower)}
-                  `
-                      )}
-                  Z
-                `}
+                      .map((d) => `L ${xScale(d.index)} ${yScale(d.yhat_lower)}`)} Z`}
                     fill={dataType.color}
                     opacity="0.3"
                   />
@@ -296,6 +319,46 @@ export default function TimeseriesChart(props: {
                   fill="none"
                   stroke={dataType.color}
                   strokeWidth="3"
+                />
+              </>
+            );
+          })}
+          {scale > 1 && (
+            <rect
+              x={(nowIndex / length) * width}
+              y={height - scrollWidth}
+              height={scrollWidth}
+              width={width / scale}
+              fill="var(--scroll-color)"
+            />
+          )}
+          {/* givenData dots */}
+          {selectedGivenData.map((data: GivenData, i: number) => {
+            const realIndex = nowIndex + i;
+            return (
+              <>
+                {/* <circle
+                className="cy-transition"
+                cx={xScale(i)}
+                cy={divided ? trendScale(data.y) : yScale(data.y)}
+                r={3}
+                fill={"var(--foreground)"}
+              /> */}
+                <rect
+                  className="y-transition"
+                  x={xScale(realIndex) - ((1 / (scaledLength - 1)) * (width - 2 * padding)) / 4}
+                  y={yScale(Math.max(data.y, data.start))}
+                  height={Math.max(1, Math.abs(yScale(data.start) - yScale(data.y)))}
+                  width={((1 / (scaledLength - 1)) * (width - 2 * padding)) / 2}
+                  fill={data.start > data.y ? "var(--chart-blue)" : "var(--chart-red)"}
+                />
+                <rect
+                  className="y-transition"
+                  x={xScale(realIndex)}
+                  y={yScale(data.upper)}
+                  height={yScale(data.lower) - yScale(data.upper)}
+                  width={1}
+                  fill={data.start > data.y ? "var(--chart-blue)" : "var(--chart-red)"}
                 />
               </>
             );
